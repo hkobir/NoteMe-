7864cc420eb66a38c32d4fefb908cd80a9e39567
hkobir <hkobir10@gmail.com>
2021-11-05 15:52:36 +0600

    setup repsitory based on viewModel

---
+++ app/src/main/java/com/hk/noteme/reepository/TaskRepository.java
@@ -0,0 +1,61 @@
+package com.hk.noteme.reepository;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import com.hk.noteme.dao.TaskDao;
+import com.hk.noteme.database.TaskDatabase;
+import com.hk.noteme.models.Task;
+
+import java.util.List;
+
+public class TaskRepository {
+    public TaskDao taskDao;
+    LiveData<List<Task>> tasks;
+
+    public TaskRepository(Application application) {
+        TaskDatabase db = TaskDatabase.getDatabase(application);
+        taskDao = db.taskDao();
+        tasks = taskDao.getAllTask();
+    }
+
+    public LiveData<List<Task>> getAllTask() {
+        return tasks;
+    }
+
+    public void insertTask(final Task task) {
+        TaskDatabase.databaseWriteExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                taskDao.insertTask(task);
+            }
+        });
+    }
+
+    public void updateTask(Task task) {
+        TaskDatabase.databaseWriteExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                taskDao.updateTask(task);
+            }
+        });
+    }
+    public void deleteTask(final Task task) {
+        TaskDatabase.databaseWriteExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                taskDao.deleteTask(task);
+            }
+        });
+    }
+    public void deleteAllTask() {
+        TaskDatabase.databaseWriteExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                taskDao.deleteAll();
+            }
+        });
+    }
+}
+
---
+++ app/src/main/java/com/hk/noteme/viewmodel/TaskViewModel.java
@@ -0,0 +1,44 @@
+package com.hk.noteme.viewmodel;
+
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.hk.noteme.models.Task;
+import com.hk.noteme.reepository.TaskRepository;
+
+import java.util.List;
+
+public class TaskViewModel extends AndroidViewModel {
+    private TaskRepository taskRepository;
+    private LiveData<List<Task>> tasks;
+
+    public TaskViewModel(@NonNull Application application) {
+        super(application);
+        taskRepository = new TaskRepository(application);
+        tasks = taskRepository.getAllTask();
+    }
+
+    public LiveData<List<Task>> getAllTask() {
+        return tasks;
+    }
+
+    public void insertTask(Task task) {
+        taskRepository.insertTask(task);
+    }
+
+    public void deleteTask(Task task) {
+        taskRepository.deleteTask(task);
+    }
+
+    public void deleteAllTask() {
+        taskRepository.deleteAllTask();
+    }
+
+    public void updateTask(Task task) {
+        taskRepository.updateTask(task);
+    }
+}
