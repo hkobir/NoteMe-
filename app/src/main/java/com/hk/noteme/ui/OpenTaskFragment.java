b6ba78d33a5d547c205715d6897eb50369b1df35
hkobir <hkobir10@gmail.com>
2021-11-05 20:44:57 +0600

    task add UI are updated

--- app/src/main/java/com/hk/noteme/ui/OpenTaskFragment.java
+++ app/src/main/java/com/hk/noteme/ui/OpenTaskFragment.java
@@ -1,5 +1,9 @@
 package com.hk.noteme.ui;
 
+import static androidx.databinding.DataBindingUtil.inflate;
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,9 +15,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.hk.noteme.R;
+import com.hk.noteme.databinding.FragmentOpenTaskBinding;
 
 public class OpenTaskFragment extends Fragment {
-
+    FragmentOpenTaskBinding binding;
+    private Context context;
 
     public OpenTaskFragment() {
         // Required empty public constructor
@@ -23,12 +29,19 @@ public class OpenTaskFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_open_task, container, false);
+        binding = inflate(inflater, R.layout.fragment_open_task, container, false);
+        context = container.getContext();
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        binding.addTaskFB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(context, AddTaskActivity.class));
+            }
+        });
     }
 }
